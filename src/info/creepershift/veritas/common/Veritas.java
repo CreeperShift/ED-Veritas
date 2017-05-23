fa06819bef60f9a28684048f72f39b2682c947f7
Max <maxhenkes@gmail.com>
2017-05-23 21:04:42 +0200

    Fixed Project Setup

---
+++ src/info/creepershift/veritas/common/Veritas.java
@@ -0,0 +1,15 @@
+package info.creepershift.veritas.common;
+
+/**
+ * ED-Veritas
+ * Created by Max on 5/23/2017.
+ */
+public class Veritas {
+
+    public static void main(String[] args){
+
+
+
+    }
+
+}
---
+++ src/info/creepershift/veritas/gui/GuiApplication.java
@@ -0,0 +1,18 @@
+package info.creepershift.veritas.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * ED-Veritas
+ * Created by Max on 5/23/2017.
+ */
+public class GuiApplication extends Application{
+
+
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+
+}
